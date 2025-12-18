a05884ef49dd070e15610865858c4e7643041a6c
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2025-12-18 22:56:47 +0530

    Module version fixes 2

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,8 @@ echo "Deployment started"
 # REMOVE the venv lines (python3.12 -m venv env / source env...) if you haven't already!
 
 # Update this line to include djangorestframework
-python3.12 -m pip install Django==5.1.6 djangorestframework sqlparse==0.5.3 tzdata==2025.1
+python3.12 -m pip install Django==5.1.6 djangorestframework psycopg2-binary==2.9.11 groq==0.37.1
+
 
 python3.12 manage.py collectstatic --noinput
 
